96c9878d15b10415ecffe98cd63e961a18738331
David Tabor <dtabor3@gmail.com>
2018-12-02 18:35:00 -0600

    pypoll - complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,21 +9,52 @@ with open(csv_path) as csv_file:
     for row in csv_reader:
         data.append({"voter_id": row[0], "county": row[1], "candidate": row[2]})
 
-
 # Generate a list of the unique values of "candidate" in data
 candidates = list(set( [row["candidate"] for row in data] )) 
-print(candidates)
-
 election_results = []
 for candidate in candidates:
     election_results.append({"candidate": candidate, "votes": 0})
 
-print(election_results)
+# Count number of votes for each candidate and store in election_results
+for row in data:
+    for election_result in election_results:
+        if election_result["candidate"] == row["candidate"]:
+            election_result["votes"] += 1
 
 # Generate summary statistics
+total_vote_count = 0
+winner = ""
+winner_vote_count = 0
+for election_result in election_results:
+    total_vote_count += election_result["votes"]
+    if election_result["votes"] > winner_vote_count:
+        winner = election_result["candidate"]
+        winner_vote_count = election_result["votes"]
 
-#count = len(data)
-#print(count)
+# Print results
+print(f'Election Results')
+print(f'-------------------------')
+print(f'Total Votes: {total_vote_count}')
+print(f'-------------------------')
+for election_result in election_results:
+    print(f'{election_result["candidate"]}: ' 
+    + f'{round(election_result["votes"]/total_vote_count*100.0,5)}% '
+    + f'({election_result["votes"]})')
+print(f'-------------------------')
+print(f'Winner: {winner}')
+print(f'-------------------------')
 
-#election_results = []
-#for row in data:
+# Write data summary to file
+file = open("output.txt", "w")
+file.write(f'Election Results\n')
+file.write(f'-------------------------\n')
+file.write(f'Total Votes: {total_vote_count}\n')
+file.write(f'-------------------------\n')
+for election_result in election_results:
+    file.write(f'{election_result["candidate"]}: ' 
+    + f'{round(election_result["votes"]/total_vote_count*100.0,5)}% '
+    + f'({election_result["votes"]})\n')
+file.write(f'-------------------------\n')
+file.write(f'Winner: {winner}\n')
+file.write(f'-------------------------\n')
+file.close()
